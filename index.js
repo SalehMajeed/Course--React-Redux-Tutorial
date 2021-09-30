eddf1fe6036ae639f9dd131f445b0fc82794efff
salehmajeed <saleh.majeed8@gmail.com>
2021-09-30 19:05:10 +0530

    redux example

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('From index.js');
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const BUY_CAKE = 'BUY_CAKE';
+
+function buyCake() {
+	return {
+		type: BUY_CAKE,
+		info: 'First Redux Action',
+	};
+}
+
+const initialState = {
+	numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case BUY_CAKE:
+			return {
+				...state,
+				numOfCakes: state.numOfCakes - 1,
+			};
+		default:
+			return state;
+	}
+};
+
+const store = createStore(reducer);
+
+console.log('Initial State', store.getState());
+const unsubscribe = store.subscribe(() => console.log('Updated State', store.getState()));
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+unsubscribe();
