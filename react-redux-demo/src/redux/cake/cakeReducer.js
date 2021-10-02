17417ff61e33c9ce66772084f77ef6c3fceb820b
salehmajeed <saleh.majeed8@gmail.com>
2021-10-02 09:31:02 +0530

    buyCake click event using react-redux

---
+++ react-redux-demo/src/redux/cake/cakeReducer.js
@@ -0,0 +1,19 @@
+import { BUY_CAKE } from './cakeTypes';
+
+const initial_state = {
+	numOfCakes: 10,
+};
+
+const cakeReducer = (state = initial_state, action) => {
+	switch (action.type) {
+		case BUY_CAKE:
+			return {
+				...state,
+				numOfCakes: state.numOfCakes - 1,
+			};
+		default:
+			return state;
+	}
+};
+
+export default cakeReducer
